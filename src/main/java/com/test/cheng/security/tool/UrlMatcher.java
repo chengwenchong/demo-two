89c662b991134f9798ba332fb83856aa83863085
chengwc <chengwc@gclasscn.com>
2017-06-27 16:08:22 +0800

    添加spring security

---
+++ src/main/java/com/test/cheng/security/tool/UrlMatcher.java
@@ -0,0 +1,8 @@
+package com.test.cheng.security.tool;
+
+public interface UrlMatcher {
+	Object compile(String paramString);  
+    boolean pathMatchesUrl(Object paramObject, String paramString);  
+    String getUniversalMatchPattern();   
+    boolean requiresLowerCaseUrl();  
+}
